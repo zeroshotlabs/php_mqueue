c363651e57214e77f939fe509062a9f4e1509a54
zaunere <hans@zaunere.com>
2024-09-15 13:02:31 -0400

    update

--- clean_build.sh
+++ clean_build.sh
@@ -3,7 +3,7 @@
 PHP_CONFIG=${PHP_CONFIG:-$(which php-config 2>/dev/null)}
 
 if [ -z "$PHP_CONFIG" ]; then
-    echo 'Error: php-config not found - try:'
+    echo -e "\n === Error: php-config not found - try:"
     echo 'PHP_CONFIG=/path/to/php-config make clean all' >&2
     exit 1
 fi
