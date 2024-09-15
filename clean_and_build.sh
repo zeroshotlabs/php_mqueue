24b09ba68b7bf06312f3cf3bada11c0140128c63
zaunere <hans@zaunere.com>
2024-09-15 15:24:45 -0400

    update

--- clean_and_build.sh
+++ clean_and_build.sh
@@ -4,7 +4,7 @@ PHP_CONFIG=${PHP_CONFIG:-$(which php-config 2>/dev/null)}
 
 if [ -z "$PHP_CONFIG" ]; then
     echo -e "\n === Error: php-config not found - try:"
-    echo 'PHP_CONFIG=/path/to/php-config composer run-script post-install-cmd' >&2
+    echo ' ===   PHP_CONFIG=/path/to/php-config composer run-script post-install-cmd' >&2
     exit 1
 fi
 
